461677647740ae5e117ce939ce890f85cb29fc0f
Cheyenne <crector@cocc.edu>
2018-10-29 08:55:14 -0700

    want it to look a little better

--- empty-example/Example2.2.js
+++ empty-example/Example2.2.js
@@ -1,12 +1,13 @@
 function setup() {
-  createCanvas(900, 780);
-  }
-  function draw() {
+  createCanvas(640, 480);
+}
+
+function draw() {
   if (mouseIsPressed) {
-  fill(0);
+    fill(0);
   } else {
-  fill(255);
+    fill(255);
   }
   ellipse(mouseX, mouseY, 80, 80);
-  }
+}
   
