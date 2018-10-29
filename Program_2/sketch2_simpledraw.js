78943d015a02a499725017ba8993d83992562efa
Cheyenne <crector@cocc.edu>
2018-10-29 09:22:35 -0700

    literally changed nothing to code and it stopped working

--- Program_2/sketch2_simpledraw.js
+++ Program_2/sketch2_simpledraw.js
@@ -1,11 +1,14 @@
-
- function setup() {
-    createCanvas(displayWidth, displayHeight);
-   strokeWeight(10)
-   stroke(0);
+function setup() {
+    createCanvas(400, 400);
 }
 
-function touchMoved() {
+function draw()
+{
+   // do nothing
+}
+
+function mouseDragged() 
+{ 
+   strokeWeight(10);
    line(mouseX, mouseY, pmouseX, pmouseY);
-   return false;
 }
